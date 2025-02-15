617cfd2fb9b4e9ec23b55defee7dd5afadf1adff
Jake Kohl <jacob.jp.kohl@gmail.com>
2025-02-15 17:20:49 -0600

    Package Updates - Prettier Config - ESLint Config - Install and Configure Cypress

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginCypress from 'eslint-plugin-cypress/flat'
+import pluginChaiFriendly from 'eslint-plugin-chai-friendly'
 
 export default [
   {
@@ -10,10 +12,20 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/node_modules/**',
+      '**/public/**',
+      '**/static/**',
+      '**/tmp/**',
+    ],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+  ...pluginCypress.configs.recommended,
+  ...pluginChaiFriendly.configs.recommendedFlat,
   skipFormatting,
 ]
